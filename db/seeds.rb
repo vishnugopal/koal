6766d60017ea690d540539f2c783895fa881084a
Vishnu Gopal <vishnugopal@users.noreply.github.com>
2018-09-10 21:32:06 +0530

    Add seed data using a real story to the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+home_folder = `cd ~; pwd`.strip
+seed_data_folder = File.join(home_folder, "Downloads", "Santos_J_Romeo@Adventures_of_Me_and_Martha_Jane")
+
+if Dir.exist? seed_data_folder
+  Story.load_from_file(folder: seed_data_folder, source_type: :storiesonline, source_format: :html)
+else
+  puts "Seed data directory does not exist. Download SJR's Me & Martha Jane, unzip & put it in ~/Downloads"
+  exit 1
+end
